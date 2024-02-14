8345192a42e42959582e4a0a0723c53179cfdad9
Kris Rowe <kris@rowelink.com>
2024-02-14 14:37:51 -0600

    Add validation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const moment = require('moment');
-
 function isSunday(dateString) {
-  const parsedDate = moment.parseZone(dateString, moment.ISO_8601_STRICT);
+  const parsedDate = moment.parseZone(dateString, moment.ISO_8601);
   return parsedDate.isValid() && parsedDate.day() === 0;
 }
 
@@ -10,18 +9,26 @@ console.log(new Date());
 function testInputString(dateString) {
 	console.log('------------------------------------');
 	console.log('Testing ' + dateString);
-        console.log('is Sunday: ' + isSunday(dateString));
+	console.log('is Sunday: ' + isSunday(dateString));
 	console.log('as JS Date: ' + new Date(dateString));
-	var parsed = moment.parseZone(dateString, moment.ISO_8601_STRICT);
-        console.log('using moment.parseZone: ' + parsed.format("YYYY-MM-DD"));
-	parsed = moment.utc(dateString);
-	console.log('using moment.utc: ' + parsed.format('YYYY-DD-MM'));
+	var parsed = moment.parseZone(dateString, undefined, true);
+    console.log(`is valid per moment using parseZone: ${parsed.isValid()}`);
+	console.log('moment.parseZone then moment.format("YYYY-DD-MM"): ' + parsed.format("YYYY-MM-DD"));
+	console.log('moment.parseZone then moment.format("YYYY-MM-DD HH:mm:ss Z"): ' + parsed.format("YYYY-MM-DD HH:mm:ss Z"));
+	if (parsed.isValid()) { // the first step in this block would fail otherwise
+		parsed = moment.utc(dateString);
+		console.log(`is valid per moment using utc: ${parsed.isValid()}`);
+		console.log('moment.utc then moment.format("YYYY-DD-MM"): ' + parsed.format("YYYY-MM-DD"));
+		console.log('moment.utc then moment.format("YYYY-MM-DD HH:mm:ss Z"): ' + parsed.format("YYYY-MM-DD HH:mm:ss Z"));
+	}
 }
 
 var testString1 = '2018-02-18';
 var testString2 = '2024-02-18T00:00:00Z';
 var testString3 = '2024-02-18T00:00:00-06:00';
+var testString4 = "2/18/2024";
 
 testInputString(testString1);
 testInputString(testString2);
 testInputString(testString3);
+testInputString(testString4);
