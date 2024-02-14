14ae9233d63fb83b979a49bf094436d60dd9e3e9
Kris Rowe <kris@rowelink.com>
2024-02-14 14:52:39 -0600

    Show desired use of moment for validation.

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ function testInputString(dateString) {
 	console.log('Testing ' + dateString);
 	console.log('is Sunday: ' + isSunday(dateString));
 	console.log('as JS Date: ' + new Date(dateString));
-	var parsed = moment.parseZone(dateString, undefined, true);
+	var parsed = moment.parseZone(dateString, moment.ISO_8601);
     console.log(`is valid per moment using parseZone: ${parsed.isValid()}`);
 	console.log('moment.parseZone then moment.format("YYYY-DD-MM"): ' + parsed.format("YYYY-MM-DD"));
 	console.log('moment.parseZone then moment.format("YYYY-MM-DD HH:mm:ss Z"): ' + parsed.format("YYYY-MM-DD HH:mm:ss Z"));
-	if (parsed.isValid()) { // the first step in this block would fail otherwise
-		parsed = moment.utc(dateString);
-		console.log(`is valid per moment using utc: ${parsed.isValid()}`);
-		console.log('moment.utc then moment.format("YYYY-DD-MM"): ' + parsed.format("YYYY-MM-DD"));
-		console.log('moment.utc then moment.format("YYYY-MM-DD HH:mm:ss Z"): ' + parsed.format("YYYY-MM-DD HH:mm:ss Z"));
-	}
+	parsed = moment.utc(dateString, moment.ISO_8601);
+	console.log(`is valid per moment using utc: ${parsed.isValid()}`);
+	console.log('moment.utc then moment.format("YYYY-DD-MM"): ' + parsed.format("YYYY-MM-DD"));
+	console.log('moment.utc then moment.format("YYYY-MM-DD HH:mm:ss Z"): ' + parsed.format("YYYY-MM-DD HH:mm:ss Z"));
+
 }
 
 var testString1 = '2018-02-18';
 var testString2 = '2024-02-18T00:00:00Z';
 var testString3 = '2024-02-18T00:00:00-06:00';
+var testString3 = '2024-02-18T00:00:00-05:00';
 var testString4 = "2/18/2024";
 
 testInputString(testString1);
