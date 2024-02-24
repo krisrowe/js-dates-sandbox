8b0d6754c41dbb708a570e0b3d205855524743d7
Kris Rowe <kris@rowelink.com>
2024-02-23 22:27:21 -0600

    Improved scenarios for output

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const moment = require('moment');
+const process = require('process');
+
 function isSunday(dateString) {
   const parsedDate = moment.parseZone(dateString, moment.ISO_8601);
   return parsedDate.isValid() && parsedDate.day() === 0;
 }
-
-console.log(new Date());
+console.log('------------------------------------')
+console.log('new Date(): ' + new Date());
+console.log('moment().format(): ' + moment().format());
+console.log('moment.utc().format(): ' + moment.utc().format());
+console.log('new Date().toLocaleString(): ' + new Date().toLocaleString());
+console.log('new Date().toISOString(): ' + new Date().toISOString());
+console.log('new Date().toDateString(): ' + new Date().toDateString());
+console.log('new Date().toLocaleDateString(): ' + new Date().toLocaleDateString());
 
 function testInputString(dateString) {
 	console.log('------------------------------------');
 	console.log('Testing ' + dateString);
+	console.log('zone: ' + process.env.TZ);
 	console.log('is Sunday: ' + isSunday(dateString));
 	console.log('as JS Date: ' + new Date(dateString));
 	var parsed = moment.parseZone(dateString, moment.ISO_8601);
     console.log(`is valid per moment using parseZone: ${parsed.isValid()}`);
 	console.log('moment.parseZone then moment.format("YYYY-DD-MM"): ' + parsed.format("YYYY-MM-DD"));
 	console.log('moment.parseZone then moment.format("YYYY-MM-DD HH:mm:ss Z"): ' + parsed.format("YYYY-MM-DD HH:mm:ss Z"));
+	console.log('moment.toLocaleString: ' + parsed.toLocaleString());
 	parsed = moment.utc(dateString, moment.ISO_8601);
 	console.log(`is valid per moment using utc: ${parsed.isValid()}`);
 	console.log('moment.utc then moment.format("YYYY-DD-MM"): ' + parsed.format("YYYY-MM-DD"));
 	console.log('moment.utc then moment.format("YYYY-MM-DD HH:mm:ss Z"): ' + parsed.format("YYYY-MM-DD HH:mm:ss Z"));
-
+	console.log('moment.toLocaleString: ' + parsed.toLocaleString());
 }
 
 var testString1 = '2018-02-18';
